d3d312aef4e0fd0d1d12797266cfa0bd11725435
RealRodder <vitaly.korzyukov@gmail.com>
2015-03-25 19:56:22 +0200

    This is my first git commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+      System.out.println ("Hello, Word!");
+      System.out.println (12+20);
+      double x = 32.2;
+      String f = "Hello" + ", Word! ";
+      System.out.println (f + x);
+      int z = 256 + 3180;
+      System.out.println ("Sum of 256 and 3180 is " + z);
+      System.out.println ("Sum of 256 and 3180 is " + (256 + 3180));
+      System.out.println ("180 / 30 is " + 180/30);
+      double zz = 180/31;
+      System.out.println (zz);
+      System.out.println ("Sum of 256 and 3180 is \"");
+      System.out.println ("Sum of 256 and 3180 is \\");
+      System.out.println ("Sum of 256 and 3180 is \nand go Dali");
+      int [] h1 = {10,0,0,21,-5,58};
+      System.out.println (h1[4]);
+      System.out.println (h1[0]+" "+h1[1]+" "+h1[2]+" "+h1[3]+" "+h1[4]+" "+h1[5]);
+
+      for (int z1 = 0; z1 <= 5; z1++) {
+        System.out.println(h1[z1] + " ");
+      }
+
+      // System.out.println(args[0]); Вывод Paramstr[0];
+
+      String[] j2 = {"a", "b", "c"};
+      String[] j3 = {"a", "d", "c"};
+
+      for (int z2 = 0; z2 <= 2; z2++) {
+        System.out.print(j2 [z2] + " - " + j3 [z2] + ", ");
+      }
+    }
+}
