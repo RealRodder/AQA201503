1b8b294fe3728e9027817b38e77c34dab10cade5
RealRodder <vitaly.korzyukov@gmail.com>
2015-03-25 20:47:23 +0200

    This is my second git commit

--- src/com/company/main2.java
+++ src/com/company/main2.java
@@ -5,6 +5,15 @@ package com.company;
  */
 public class main2 {
     public static void main(String[] args) {
-
+      // Если первый элемент args равен "Hello", тогда напечатать в консоль
+      // "Class", иначе - напечатать "Good bye!"
+        if (args[0].equals ("Hello"))
+        {
+            System.out.println("Class!");
+        }
+        else
+        {
+            System.out.println("Good bye!");
+        }
     }
 }
